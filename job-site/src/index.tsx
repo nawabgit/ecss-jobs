054a398899e66d7d4f7dd9e63702d28b44ab9eea
Nawab <nawab.mir14@gmail.com>
2020-07-25 00:42:51 +0100

    added global css style for project

--- job-site/src/index.tsx
+++ job-site/src/index.tsx
@@ -2,8 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Jobs from "./Jobs";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    background: #F7F8FB;
+    font-size: 14pt;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Jobs />
   </React.StrictMode>,
   document.getElementById("root")
