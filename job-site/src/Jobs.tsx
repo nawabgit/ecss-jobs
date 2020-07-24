0a284f2068b08567ba10c43f1bc5c7312202f007
Nawab <nawab.mir14@gmail.com>
2020-07-24 22:29:00 +0100

    app renamed to job

--- job-site/src/Jobs.tsx
+++ job-site/src/Jobs.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function App() {
+function Jobs() {
   return <p>website</p>;
 }
 
-export default App;
+export default Jobs;
--- job-site/src/index.tsx
+++ job-site/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Jobs from "./Jobs";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Jobs />
   </React.StrictMode>,
   document.getElementById("root")
 );
