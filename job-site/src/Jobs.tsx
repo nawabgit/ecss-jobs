3d6578767cb006b3ebd5e59f73cba8fa4353a3b9
Nawab <nawab.mir14@gmail.com>
2020-07-26 21:13:06 +0100

    reduced intensity of details shadow

--- job-site/src/Jobs.tsx
+++ job-site/src/Jobs.tsx
@@ -149,7 +149,7 @@ const JobDetails = styled.div`
   flex: 6;
   margin: 1em;
   background-color: #fafafa;
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 `;
 
 const JobIcons = styled.div`
