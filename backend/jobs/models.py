1cb45b84e176fe9bb9d0c0e050d817a56c08081b
Nawab <nawab.mir14@gmail.com>
2020-07-29 01:01:59 +0100

    added and registered listing model

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+# Create db models here
+
+class Listing(models.Model):
+    company = models.CharField(max_length=60)
+    role = models.CharField(max_length=60)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    full_salary = models.CharField(max_length=60)
+    salary_preview = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    greater_location = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.company} {self.role} {date}"
