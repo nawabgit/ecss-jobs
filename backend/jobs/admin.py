517211a6c28b33a7af27458f9284f91f05d9039d
Nawab <nawab.mir14@gmail.com>
2020-07-31 01:15:06 +0100

    added two new models - Company, Sponsor

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Listing
+from .models import Listing, Company, Sponsor
 
 # Register models here
 admin.site.register(Listing)
+admin.site.register(Company)
+admin.site.register(Sponsor)
--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -4,29 +4,51 @@ from datetime import datetime
 # Create db models here
 
 class Listing(models.Model):
-    # Lets be a little conservative and only use 2 bytes
     JOB_TYPE_CHOICES = [
-        ('C', 'Competition'),
-        ('GS', 'Grant/Scholarship'),
-        ('I', 'Internship'),
-        ('P', 'Placement'),
-        ('PT', 'Part Time'),
-        ('FT', 'Full Time'),
-        ('G', 'Graduate'),
+        ('competition', 'Competition'),
+        ('grant', 'Grant'),
+        ('scholarship', 'Scholarship'),
+        ('internship', 'Internship'),
+        ('placement', 'Placement'),
+        ('part-time', 'Part Time'),
+        ('full-time', 'Full Time'),
+        ('graduate', 'Graduate'),
     ]
 
-    company = models.CharField(max_length=60, help_text="E.g. btecSolutions")
-    image = models.FileField(upload_to="images/", blank=True, help_text="SVG preferably - 40x40 Pixels only or distortion will occur")
+    company = models.CharField(max_length=60)
     role = models.CharField(max_length=60, help_text="E.g. Software Engineer")
     date = models.DateTimeField(default=datetime.now, help_text="The date and time the listing will be posted")
     full_salary = models.CharField(max_length=60, help_text="E.g. '£30,000 - £40,000 p.a.' or '£12.50 p.h.'")
     salary_preview = models.CharField(max_length=20, help_text="E.g. '£30K - £40K' or '£12.50/HR'")
     location = models.CharField(max_length=20, help_text="E.g. London")
     greater_location = models.CharField(max_length=20, null=True, blank=True, help_text="E.g. England")
-    job_type = models.CharField(max_length=2, choices=JOB_TYPE_CHOICES)
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE_CHOICES)
     apply_url = models.URLField(help_text="The URL that the apply button will reroute to")
-    mailto = models.URLField(max_length=255, help_text="The mailto link (preferably with ECSS in subject)")
-    description = models.CharField(max_length=4000, help_text="The Markdown job description (max 4000 characters) See: https://www.markdownguide.org/basic-syntax/")
+    mailto = models.TextField(help_text="The mailto link (preferably with ECSS in subject)")
+    description = models.TextField(help_text="The Markdown job description (max 4000 characters) See: https://www.markdownguide.org/basic-syntax/")
 
     def __str__(self):
         return f"{self.company} {self.role} {self.date}"
+
+class Company(models.Model):
+    name = models.CharField(max_length=60, help_text="E.g. btecSolutions")
+    image = models.FileField(upload_to="images/", blank=True, help_text="SVG highly preferable - 1:1 aspect ratio only")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Sponsor(models.Model):
+    SPONSOR_TYPE_CHOICES = [
+        ('gold', 'Gold'),
+        ('silver', 'Silver'),
+        ('bronze', 'Bronze'),
+    ]
+
+    company = models.OneToOneField(Company, on_delete=models.CASCADE, help_text="The sponsor must be an existing Company")
+    sponsor_level = models.CharField(max_length=20, choices=SPONSOR_TYPE_CHOICES)
+
+    description = models.TextField(help_text="A paragraph or so describing the company")
+    website = models.URLField(help_text="A URL to the company's website")
+
+    def __str__(self):
+        return f"{self.company.name}"
