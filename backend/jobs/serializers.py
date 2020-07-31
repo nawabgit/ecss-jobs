d3ff925ab4837d27ac366f0115490341551e865f
Nawab <nawab.mir14@gmail.com>
2020-07-31 02:55:22 +0100

    added serializers

--- backend/jobs/serializers.py
+++ backend/jobs/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from .models import Listing
+from .models import Listing, Company, Sponsor
 
 class ListingSerializer(serializers.HyperlinkedModelSerializer):
+    company = serializers.SerializerMethodField()
+    
+    def get_company(self, obj):
+        if hasattr(obj.company, 'sponsor'):
+            return SponsorSerializer(obj.company.sponsor).data
+        else:
+            return CompanySerializer(obj.company).data
+
     class Meta:
         model = Listing
-        fields = ('company', 'image', 'role', 'date', 'full_salary' , 'salary_preview' , 'location' , 'greater_location' , 'job_type', 'apply_url', 'mailto', 'description')
+        fields = ('company', 'role', 'date', 'full_salary' , 'salary_preview' , 'location' , 'greater_location' , 'job_type', 'apply_url', 'mailto', 'description')
+
+
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('name', 'image')
+
+        
+class SponsorSerializer(serializers.HyperlinkedModelSerializer):
+    # We could just use the CompanySerializer to retrieve these,
+    #  but then they are nested
+    name = serializers.CharField(source='company.name')
+    image = serializers.FileField(source='company.image')
+
+    class Meta:
+        model = Sponsor
+        fields = ('name', 'image', 'sponsor_level', 'description', 'website')
