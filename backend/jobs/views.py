aebea26832d70cfeabf2c3d18e17d60a46e64c17
Nawab <nawab.mir14@gmail.com>
2020-07-31 02:53:42 +0100

    added views for Company and Sponsor

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 
-from .serializers import ListingSerializer
-from .models import Listing
+from .serializers import ListingSerializer, CompanySerializer, SponsorSerializer
+from .models import Listing, Company, Sponsor
 
 # Create views here
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all().order_by('date')
     serializer_class = ListingSerializer
+    http_method_names = ['get']
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    http_method_names = ['get']
+
+    
+class SponsorViewSet(viewsets.ModelViewSet):
+    queryset = Sponsor.objects.all()
+    serializer_class = SponsorSerializer
     http_method_names = ['get']
