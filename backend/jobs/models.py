32730fd88774cdb5757a466d14bcea093a2f7931
Nawab <nawab.mir14@gmail.com>
2020-07-31 02:02:29 +0100

    models moved aroud and listing now has onetoone

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -3,39 +3,13 @@ from datetime import datetime
 
 # Create db models here
 
-class Listing(models.Model):
-    JOB_TYPE_CHOICES = [
-        ('competition', 'Competition'),
-        ('grant', 'Grant'),
-        ('scholarship', 'Scholarship'),
-        ('internship', 'Internship'),
-        ('placement', 'Placement'),
-        ('part-time', 'Part Time'),
-        ('full-time', 'Full Time'),
-        ('graduate', 'Graduate'),
-    ]
-
-    company = models.CharField(max_length=60)
-    role = models.CharField(max_length=60, help_text="E.g. Software Engineer")
-    date = models.DateTimeField(default=datetime.now, help_text="The date and time the listing will be posted")
-    full_salary = models.CharField(max_length=60, help_text="E.g. '£30,000 - £40,000 p.a.' or '£12.50 p.h.'")
-    salary_preview = models.CharField(max_length=20, help_text="E.g. '£30K - £40K' or '£12.50/HR'")
-    location = models.CharField(max_length=20, help_text="E.g. London")
-    greater_location = models.CharField(max_length=20, null=True, blank=True, help_text="E.g. England")
-    job_type = models.CharField(max_length=20, choices=JOB_TYPE_CHOICES)
-    apply_url = models.URLField(help_text="The URL that the apply button will reroute to")
-    mailto = models.TextField(help_text="The mailto link (preferably with ECSS in subject)")
-    description = models.TextField(help_text="The Markdown job description (max 4000 characters) See: https://www.markdownguide.org/basic-syntax/")
-
-    def __str__(self):
-        return f"{self.company} {self.role} {self.date}"
-
 class Company(models.Model):
     name = models.CharField(max_length=60, help_text="E.g. btecSolutions")
     image = models.FileField(upload_to="images/", blank=True, help_text="SVG highly preferable - 1:1 aspect ratio only")
 
     def __str__(self):
         return f"{self.name}"
+
 
 class Sponsor(models.Model):
     SPONSOR_TYPE_CHOICES = [
@@ -52,3 +26,31 @@ class Sponsor(models.Model):
 
     def __str__(self):
         return f"{self.company.name}"
+
+
+class Listing(models.Model):
+    JOB_TYPE_CHOICES = [
+        ('competition', 'Competition'),
+        ('grant', 'Grant'),
+        ('scholarship', 'Scholarship'),
+        ('internship', 'Internship'),
+        ('placement', 'Placement'),
+        ('part-time', 'Part Time'),
+        ('full-time', 'Full Time'),
+        ('graduate', 'Graduate'),
+    ]
+
+    company = models.OneToOneField(Company, on_delete=models.CASCADE)
+    role = models.CharField(max_length=60, help_text="E.g. Software Engineer")
+    date = models.DateTimeField(default=datetime.now, help_text="The date and time the listing will be posted")
+    full_salary = models.CharField(max_length=60, help_text="E.g. '£30,000 - £40,000 p.a.' or '£12.50 p.h.'")
+    salary_preview = models.CharField(max_length=20, help_text="E.g. '£30K - £40K' or '£12.50/HR'")
+    location = models.CharField(max_length=20, help_text="E.g. London")
+    greater_location = models.CharField(max_length=20, null=True, blank=True, help_text="E.g. England")
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE_CHOICES)
+    apply_url = models.URLField(help_text="The URL that the apply button will reroute to")
+    mailto = models.CharField(max_length=255, help_text="The mailto link (preferably with ECSS in subject)")
+    description = models.TextField(help_text="The Markdown job description. See: https://www.markdownguide.org/basic-syntax/")
+
+    def __str__(self):
+        return f"{self.company} {self.job_type} {self.role} {self.date}"
